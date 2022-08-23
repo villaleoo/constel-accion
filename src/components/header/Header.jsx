538c3a152b11119a0a0f610e46154411db49e950
leopoldo <villaleoo@hotmail.com>
2022-08-23 11:14:24 -0300

    comenzando el header, agregando sass

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import './headerStyles.scss'
 
 export const Header = () => {
   return (
-    <div>
-        
+    <div className='w-100 fixed-top headerContainer'>
+        <h1>constelaccion</h1>
     </div>
   )
 }
--- src/components/header/navbar/Navbar.jsx
+++ src/components/header/navbar/Navbar.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+        
+    </div>
   )
 }
