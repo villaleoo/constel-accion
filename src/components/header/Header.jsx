e36bd06ac6e8873cb326b799fd4afda63970e3f4
leopoldo <villaleoo@hotmail.com>
2022-08-31 21:24:48 -0300

    trabajando header y navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './headerStyles.scss'
+import { Navbar } from './navbar/Navbar'
 
 export const Header = () => {
   return (
-    <div className='w-100 fixed-top headerContainer'>
-        <h1>constelaccion</h1>
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-light headerContainer">
+      <div className='headerSite'>
+        <div className="navbar-brand logoSite" href="#">CONSTELACCION</div>
+        <button className="navbar-toggler" 
+                type="button" 
+                data-toggle="collapse" 
+                data-target="#navbarNavAltMarkup" 
+                aria-controls="navbarNavAltMarkup" 
+                aria-expanded="false" 
+                aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      </div>
+        <Navbar/>
+    </nav>
   )
 }
--- src/components/header/navbar/Navbar.jsx
+++ src/components/header/navbar/Navbar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import './navbarStyles.scss'
 
 export const Navbar = () => {
   return (
-    <div>
-        
-    </div>
+        <div class="collapse navbar-collapse navbarContainer" id="navbarNavAltMarkup">
+          <div class="navbar-nav navListContainer">
+            <a class="nav-link active" href="#">Servicios <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="#">Marca</a>
+            <a class="nav-link" href="#">Colaboradores</a>
+            <a class="nav-link" href="#">Experiencia</a>
+            <a class="nav-link">Contacto</a>
+          </div>
+        </div>
   )
 }
