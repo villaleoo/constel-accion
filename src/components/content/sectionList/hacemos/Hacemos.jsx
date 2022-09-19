27f21fb7d1d8d6158d05c07b9da8fc8f0fafdb62
leopoldo <villaleoo@hotmail.com>
2022-09-18 23:28:08 -0300

    trabajando servicios

--- src/components/content/sectionList/hacemos/Hacemos.jsx
+++ src/components/content/sectionList/hacemos/Hacemos.jsx
@@ -1,89 +1,68 @@
 import React from 'react'
+import { useContext } from 'react'
 import { useState } from 'react'
+import { GlobalContextProvider } from '../../../../context/GlobalProvider'
 import './hacemosStyles.scss'
 
 export const Hacemos = () => {
-    const [buttonChecked, setButtonChecked] = useState('investigacion')
+    const [buttonChecked, setButtonChecked] = useState('investigacion');
+    const {serviceList, serviceContent} = useContext(GlobalContextProvider);
+
+
 
     function handleChangeRadio (e) {
         setButtonChecked(e.target.value)
     }
 
+   
 
   return (
     <div className='hacemosContainer'>
+      
         <div className='hacemosInputs'>
-            <label htmlFor=""></label>
-            <input 
-                type="radio" 
-                value='investigacion' 
-                name="radioServicios" 
-                id=""
-                checked={buttonChecked === 'investigacion' ? true : false}
-                onChange={handleChangeRadio} 
-            />
-            <label htmlFor=""></label>
-            <input 
-                type="radio" 
-                value ='branding' 
-                name="radioServicios" 
-                id=""
-                checked={buttonChecked === 'branding' ? true : false} 
-                onChange={handleChangeRadio}  
-            />
-            <label htmlFor=""></label>
-            <input 
-                type="radio" 
-                value='comunicacion' 
-                name="radioServicios" 
-                id=""
-                checked={buttonChecked === 'comunicacion' ? true : false} 
-                onChange={handleChangeRadio}  
-            />
-            <label htmlFor=""></label>
-            <input 
-                type="radio" 
-                value='innovacion'
-                name="radioServicios" 
-                id=""
-                checked={buttonChecked === 'innovacion' ? true : false}
-                onChange={handleChangeRadio}   
-            />
-            <label htmlFor=""></label>
-            <input 
-                type="radio"
-                value='transformacion' 
-                name="radioServicios" 
-                id=""
-                checked={buttonChecked === 'transformacion' ? true : false}
-                onChange={handleChangeRadio}   
-            />
+            {   serviceList.map((serviceItem, index)=>
+            
+            <div className='inputsContainer'>
+                <input
+                    key={`input${index}`}
+                    type='radio'
+                    value={serviceItem}
+                    name='radioServices'
+                    id={`serviceInput${index}`}
+                    checked={buttonChecked === serviceItem ? true : false}
+                    onChange={handleChangeRadio}
+                
+                />
+                <label key={`label${index}`} htmlFor={`serviceInput${index}`}></label>
+                {
+                    buttonChecked === serviceList[index] &&
+                <label className='labelInputs' key={index}>{serviceList[index]}</label>
+                }
+            </div>
+                
+            
+            )
+            }
         </div>
         <div className='hacemosContent'>
             <h3>HACEMOS</h3>
-        { buttonChecked === 'investigacion' &&
-        <p>esto es investigacion</p>
+            {
 
-        }
-
-        { buttonChecked === 'branding' && 
-        <p>esto es branding</p>
-
-        }
-
-        { buttonChecked === 'comunicacion' &&
-        <p>esto es comunicacion</p>
-
-        }
-
-        { buttonChecked === 'innovacion' &&
-        <p>esto es innovacion</p>
-
-        }
-        { buttonChecked === 'transformacion' &&
-        <p>esto es transformacion</p>
-
-        }
+                serviceContent.map((serviceActive, index)=>
+                buttonChecked === serviceContent[index].service && 
+                
+                <>
+                <label key={index}>{serviceContent[index].service
+                }</label>
+                    <h2 key={index}>{`${serviceContent[index].title}`}</h2>
+                    <p key={index}>
+                        {`${serviceContent[index].text}`}
+                    </p>
+                    <p className="keywords" key={index}>{serviceContent[index].keywords}</p>
+                </>
+                
+                )
+            }
         </div>
     </div>
   )
--- src/context/GlobalProvider.jsx
+++ src/context/GlobalProvider.jsx
@@ -5,14 +5,55 @@ export const GlobalContextProvider = createContext('');
 
 export const GlobalProvider = ({children}) => {
 
-    let navbarList= ['Hacemos', 'Somos', 'Creamos','Contacto']
+    let navbarList= ['Hacemos', 'Somos', 'Creamos','Contacto'];
 
+    let serviceList= ['investigacion', 'branding', 'comunicacion', 'innovacion', 'transformacion' ];
+
+    
+    let serviceContent= [
+      {
+        service:'investigacion',
+        title:`Investigación`,
+        text:` Para entender, explorar y descubrir consumos, contextos, necesidades y comportamientos, y convertir esos aprendizajes en acciones que diferencien, acerquen y hagan crecer a tu negocio.
+      Usamos técnicas mixtas que mejor se adapten a la necesidad y desafio de investigacion.`,
+        keywords: `Consumos • Mercado • Cultura • Problemas & Oportunidades • Deseos y necesidades • Segmentos y arquetipos • Salud de marca • Validaciones •`,
+      },
+      {
+        service:'branding',
+        title:`Estrategia y diseño de marca`,
+        text:` Para hacer diferente, relevante  mas competitiva la propuesta de tu marca, sea en su creación o para su actualización.
+        Definimos, diseñamos y creamos el enfoque estratégico y los elementos de la experiencia de marca para impulsar el crecimiento y desarrollo de tu negocio.`,
+        keywords:`Propuestas de valor • Propósito de marca • Conceptos • Punto de vista • Valores y pilares • Manuales de voz, tono y estilo • Universos virtuales y verbales • Experiencias de marca off/on/hibridas •`
+      },
+      {
+        service:'comunicacion',
+        title:`Estrategias de comunicacion y contenido`,
+        text:` Para transmitir y hacer llegar los mensajes de tu marca de manera clara, diferente y consistente, en los términos y condiciones de los consumidores.
+        Definimos y creamos, con estrategia y creatividad, la comunicacion y contenidos de tu marca, para que conecte con ellos y se obtengan mejores resultados.`,
+        keywords:`Conceptos de comunicación • Arquitectura de mensajes • Pilares & manuales de contenido y moderación • Ecosistema & roles de canales • Estrategias de segmentación • Growth Marketing • Inbound Marketing • Campañas publicitarias • Contenido creativo •`
+
+      },
+      {
+        service:'innovacion',
+        title:'Innovación de negocio, productos y servicios',
+        text:` Para crear y/o evolucionar en un contexto que necesita continua actualización y en el que los consumidores son más exigentes y menos leales.
+        Colaboramos en encontrar, crear y llevar a la vida nuevas fuentes de crecimiento de negocio que respondan a necesidades y deseos reales de los consumidores.`,
+        keywords:`Exploración de oportunidades y problemas • Territorios de innovación • Ideas y conceptos de productos, servicios y experiencias • Modelos de negocio y relacionamiento • Customer Journeys & Blueprints de compra y uso • Validaciones • Roadmaps y project pianning •`,
+      },
+      {
+        service:'transformacion',
+        title:`Workshops transformativos`,
+        text:`Para pensar juntos de manera estrategica, unir fuerzas e ideales en resolver problemas, crear los próximos planes estratégicos, verbalizar el propósito y valores de la organización, priorizar temas y proyectos, o lo que se necesite hacer.
+        Acompañamos e impulsamos a los equipos a ir más allá de lo de toda la vida y del confort, sin dejar de estar alineados a la estrategia del negocio y a los consumidores.`,
+        keywords:`Talleres de co-reflexión y co-ideación • Encuentros transformativos • Sesiones de alineación estratégica • Retrospectivas • Sesiones de priorización y consenso • Formaciones •`,
+      }
+    ]
 
 
 
 
   return (
-    <GlobalContextProvider.Provider value={{navbarList,}}>
+    <GlobalContextProvider.Provider value={{navbarList,serviceList, serviceContent}}>
         {children}
     </GlobalContextProvider.Provider>
   )
